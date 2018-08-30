d0e49b649f513e7daff9dd622595e061079722f2
Jesus Enrique Romero Paz <jesusenrique150620@gmail.com>
2018-08-29 20:43:22 -0400

    Exercise 2

---
+++ exercise2.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""
+
+Prompt a user to enter in an IP address from standard input. Read the IP address in and break it
+up into its octets. Print out the octets in decimal, binary, and hex.
+
+Your program output should look like the following:
+
+$ python exercise2.py
+Please enter an IP address: 80.98.100.240
+
+    Octet1         Octet2         Octet3         Octet4
+------------------------------------------------------------
+      80             98             100            240
+   0b1010000      0b1100010      0b1100100     0b11110000
+     0x50           0x62           0x64           0xf0
+------------------------------------------------------------
+
+Four columns, fifteen characters wide, a header column, data centered in the column.
+
+"""
+
+from __future__ import print_function
+
+try: 
+	#Python 2
+	ip_addr = raw_input("Please enter an IP address: ")
+except NameError:
+	#Python 3
+	ip_addr = input("Please enter an IP address: ")
+
+octet = ip_addr.split(".")
+
+print("\n")
+print("{:^20}{:^20}{:^20}{:^20}".format("Octet1", "Octet2", "Octet3", "Octet4"))
+print("-" * 80)
+print("{:^20}{:^20}{:^20}{:^20}".format(*octet))
+print("{:^20}{:^20}{:^20}{:^20}".format(bin(int(octet[0])), bin(int(octet[1])), bin(int(octet[2])), bin(int(octet[3]))))
+print("{:^20}{:^20}{:^20}{:^20}".format(hex(int(octet[0])), hex(int(octet[1])), hex(int(octet[2])), hex(int(octet[3]))))
+print("-" * 80)
+
+
+

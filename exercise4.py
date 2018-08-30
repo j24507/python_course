05a8aad5d704249fd4be3a192e9a21adfdf06108
Jesus Enrique Romero Paz <jesusenrique150620@gmail.com>
2018-08-30 12:56:57 -0400

    Exercise 4

---
+++ exercise4.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# This was the shebang line
+
+"""
+Create a show_version variable that contains the following
+
+show_version = "*0  	  CISCO881-SEC-K9       FTX0000038X    "
+
+Remove all leading and trailing whitespace from the string.
+Split the string and extract the model and serial_number from it.
+Check if 'Cisco' is contained in the model string (ignore capitalization).
+Check if '881' is in the model string.
+Print out both the serial number and the model.
+"""
+
+from __future__ import print_function, unicode_literals
+#For compatibility of the print() function and make byte strings unicode strings (unicode_literals)
+
+show_version = "*0        CISCO881-SEC-K9      FTX0000038X    "
+show_version = show_version.strip()
+
+fields = show_version.split()
+model = fields[1]
+serial = fields[2]
+
+print()
+vendor = "cisco" in model.lower()
+print("Checking if the model contains cisco: {}".format(vendor))
+
+print()
+model_881 = "881" in model
+print("Checking if model contains 881: {}".format(model_881))
+
+print()
+print("Serial Number: {}".format(serial))
+print()
+print("Model: {}".format(model))
+
+

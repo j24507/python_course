6f9d04977649e5f074a32ea0806972f8fe7c0d36
Jesus Enrique Romero Paz <jesusenrique150620@gmail.com>
2018-08-29 20:59:11 -0400

    exercise 3 no completed

---
+++ exercise3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# That was the shebang line
+
+from __future__ import print_function, unicode_literals
+
+ipv6_addr1 = "2001:db8::1:1"
+IPV6_ADDR2 = "2001:db8::1:2"
+IpV6_addr3 = "2001:db8::1:3"
